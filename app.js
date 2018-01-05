76662dc636097e119a4523a3f64e7804cdd77c68
Georgi Ivanov <georgitivanov@gmail.com>
2018-01-05 11:05:12 +0200

    Initiating first view

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
+/*
+    This file is generated and updated by Sencha Cmd. You can edit this file as
+    needed for your application, but these edits will have to be merged by
+    Sencha Cmd when it performs code generation tasks such as generating new
+    models, controllers or views and when running "sencha app upgrade".
+
+    Ideally changes to this file would be limited and most work would be done
+    in other places (such as Controllers). If Sencha Cmd cannot merge your
+    changes and its generated code, it will produce a "merge conflict" that you
+    will need to resolve manually.
+*/
+
 Ext.application({
-    name: 'Touch App',
+    name: 'TouchApp',
+
+    requires: [
+        'Ext.MessageBox'
+    ],
+
+    views: [
+        'Main'
+    ],
+
+    icon: {
+        '57': 'resources/icons/Icon.png',
+        '72': 'resources/icons/Icon~ipad.png',
+        '114': 'resources/icons/Icon@2x.png',
+        '144': 'resources/icons/Icon~ipad@2x.png'
+    },
+
+    isIconPrecomposed: true,
+
+    startupImage: {
+        '320x460': 'resources/startup/320x460.jpg',
+        '640x920': 'resources/startup/640x920.png',
+        '768x1004': 'resources/startup/768x1004.png',
+        '748x1024': 'resources/startup/748x1024.png',
+        '1536x2008': 'resources/startup/1536x2008.png',
+        '1496x2048': 'resources/startup/1496x2048.png'
+    },
 
     launch: function() {
         Ext.create("Ext.tab.Panel", {
@@ -8,19 +46,58 @@ Ext.application({
 
             items: [
                 {
-                    title: 'Home',
-                    iconCls: 'home',
-                    cls: 'home',
+                    xtype: 'nestedlist',
+                    title: 'Blog',
+                    iconCls: 'star',
+                    displayField: 'title',
 
-                    html: [
-                        '',
-                        'Welcome to Sencha Touch',
-                        "We're creating the Getting Started app, which demonstrates how ",
-                        "to use tabs, lists, and forms to create a simple app.",
-                        'Sencha Touch'
-                    ].join("")
+                    store: {
+                        type: 'tree',
+
+                        fields: [
+                            'title', 'link', 'author', 'contentSnippet', 'content',
+                            {name: 'leaf', defaultValue: true}
+                        ],
+
+                        root: {
+                            leaf: false
+                        },
+
+                        proxy: {
+                            type: 'jsonp',
+                            url: 'http://robocipe.com/touchapp/feed.json',
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'responseData.feed.entries'
+                            }
+                        }
+                    },
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    }
                 }
             ]
         });
+    },
+
+    onUpdated: function() {
+        Ext.Msg.confirm(
+            "Application Update",
+            "This application has just successfully been updated to the latest version. Reload now?",
+            function(buttonId) {
+                if (buttonId === 'yes') {
+                    window.location.reload();
+                }
+            }
+        );
     }
 });
