4e435bc6ad37df7acd101414cfce99a201e12e38
Georgi Ivanov <georgitivanov@gmail.com>
2018-01-05 11:14:23 +0200

    Initiating first view

--- app.js
+++ app.js
@@ -1,77 +1,51 @@
-/*
-    This file is generated and updated by Sencha Cmd. You can edit this file as
-    needed for your application, but these edits will have to be merged by
-    Sencha Cmd when it performs code generation tasks such as generating new
-    models, controllers or views and when running "sencha app upgrade".
-
-    Ideally changes to this file would be limited and most work would be done
-    in other places (such as Controllers). If Sencha Cmd cannot merge your
-    changes and its generated code, it will produce a "merge conflict" that you
-    will need to resolve manually.
-*/
-
+// We've added a third and final item to our tab panel - scroll down to see it
 Ext.application({
-    name: 'TouchApp',
-
-    requires: [
-        'Ext.MessageBox'
-    ],
-
-    views: [
-        'Main'
-    ],
-
-    icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
-    },
-
-    isIconPrecomposed: true,
-
-    startupImage: {
-        '320x460': 'resources/startup/320x460.jpg',
-        '640x920': 'resources/startup/640x920.png',
-        '768x1004': 'resources/startup/768x1004.png',
-        '748x1024': 'resources/startup/748x1024.png',
-        '1536x2008': 'resources/startup/1536x2008.png',
-        '1496x2048': 'resources/startup/1496x2048.png'
-    },
+    name: 'Sencha',
 
     launch: function() {
+
+        Ext.create('Ext.data.TreeStore', {
+            storeId: 'TreeStore',
+            fields: ['title', 'link', 'author', 'contentSnippet', 'content', {
+                name: 'leaf',
+                defaultValue: true
+            }],
+            root: {
+                leaf: false
+            },
+            proxy: {
+                type: 'jsonp',
+                url: 'http://robocipe.com/touchapp/feed.json',
+                reader: {
+                    type: 'json',
+                    rootProperty: 'responseData.feed.entries'
+                }
+            }
+        });        
         Ext.create("Ext.tab.Panel", {
             fullscreen: true,
             tabBarPosition: 'bottom',
 
             items: [
                 {
+                    title: 'Home',
+                    iconCls: 'home',
+                    cls: 'home',
+                    html: [
+                        '',
+                        'Welcome to Sencha Touch',
+                        "We're creating the Getting Started app, which demonstrates how ",
+                        "to use tabs, lists, and forms to create a simple app.",
+                        'Sencha Touch'
+                    ].join("")
+                },
+                {
                     xtype: 'nestedlist',
                     title: 'Blog',
                     iconCls: 'star',
                     displayField: 'title',
 
-                    store: {
-                        type: 'tree',
-
-                        fields: [
-                            'title', 'link', 'author', 'contentSnippet', 'content',
-                            {name: 'leaf', defaultValue: true}
-                        ],
-
-                        root: {
-                            leaf: false
-                        },
-
-                        proxy: {
-                            type: 'jsonp',
-                            url: 'http://robocipe.com/touchapp/feed.json',
-                            reader: {
-                                type: 'json',
-                                rootProperty: 'responseData.feed.entries'
-                            }
-                        }
-                    },
+                    store: 'TreeStore',
 
                     detailCard: {
                         xtype: 'panel',
@@ -84,20 +58,47 @@ Ext.application({
                             this.getDetailCard().setHtml(post.get('content'));
                         }
                     }
+                },
+                // this is the new item
+                {
+                    title: 'Contact',
+                    iconCls: 'user',
+                    xtype: 'formpanel',
+                    url: 'contact.php',
+                    layout: 'vbox',
+
+                    items: [
+                        {
+                            xtype: 'fieldset',
+                            title: 'Contact Us',
+                            instructions: '(email address is optional)',
+                            height: 285,
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    label: 'Name'
+                                },
+                                {
+                                    xtype: 'emailfield',
+                                    label: 'Email'
+                                },
+                                {
+                                    xtype: 'textareafield',
+                                    label: 'Message'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Send',
+                            ui: 'confirm',
+                            handler: function() {
+                                this.up('formpanel').submit();
+                            }
+                        }
+                    ]
                 }
             ]
         });
-    },
-
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
     }
 });
